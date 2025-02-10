03c10d292b3dab4c5829dd546e0dd417c07e5624
Chilakala DharmikaDevi <106511730+dharmii@users.noreply.github.com>
2025-02-10 23:24:44 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,38 +1,3 @@
-// const {
-//     GoogleGenerativeAI,
-//     HarmCategory,
-//     HarmBlockThreshold,
-//   } = from("@google/generative-ai");
-  
-//   const apiKey = "AIzaSyBGbpEPwHPaT2kVwXh6_YcMS26XIyPHpEE"
-//   const genAI = new GoogleGenerativeAI(apiKey);
-  
-//   const model = genAI.getGenerativeModel({
-//     model: "gemini-2.0-flash",
-//   });
-  
-//   const generationConfig = {
-//     temperature: 1,
-//     topP: 0.95,
-//     topK: 40,
-//     maxOutputTokens: 8192,
-//     responseMimeType: "text/plain",
-//   };
-  
-//   async function run() {
-//     const chatSession = model.startChat({
-//       generationConfig,
-//       history: [
-//       ],
-//     });
-  
-//     const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
-//     console.log(result.response.text());
-//   }
-  
-//   run();
-
-
 import {
   GoogleGenerativeAI,
   HarmCategory,
